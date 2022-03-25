ea760a6c76bf4b2acaa71eada0b739c06c05b568
kiminsun1234 <101077689+kiminsun1234@users.noreply.github.com>
2022-03-25 21:38:56 +0900

    scrollevent add

--- js/index2.js
+++ js/index2.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     scroll();
     slide();
     sec_top();
+    scrolltext();
 });
 
 function scroll(){
@@ -47,5 +48,26 @@ function sec_top(){
         var $height = $("header").height();
         var $offset = $($href).offset().top-$height;
         $("html").stop().animate({scrollTop:$offset},1000);
+    });
+}
+function scrolltext(){
+    var $sub_01 = $(".sub_01");
+        $list_01 = $(".list_01");
+        $list_02 = $(".standard");
+        $offset = 500;
+        $sub_01off = $sub_01.offset().top - $offset;
+        $list_01off = $list_01.offset().top - $offset;
+        $list_02off = $list_02.offset().top - $offset;
+        console.log($list_02off);
+    $(window).scroll(function(){
+        if($(this).scrollTop() > $sub_01off){
+            $sub_01.find(".sub_introsm").addClass("animate");
+        };
+        if($(this).scrollTop() > $list_01off){
+            $list_01.find("div").addClass("right");
+        };
+        if($(this).scrollTop() > $list_02off){
+            $list_02.find("div").addClass("left");
+        };
     });
 }
